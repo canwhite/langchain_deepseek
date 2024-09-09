9cbb2769336389875f30ebfc08be3669ce8d52ee
qiaochao <qiaochao@icourt.cc>
2024-09-09 14:18:45 +0800

    feat: prompts principle

---
+++ prompts/tool.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+# os.path.abspath(__file__) 获取当前文件的绝对路径。
+# os.path.dirname() 获取指定路径的目录部分。
+# 通过两次调用 os.path.dirname()，我们得到了当前文件的父目录的父目录。
+# 最后，使用 sys.path.append() 将这个路径添加到系统路径中。
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from llm_ds import SingletonChatOpenAI
+
+llm = SingletonChatOpenAI().llm
+
+
+def get_completion(prompt):
+    result =  llm.invoke(prompt)
+    return result.content
+
+

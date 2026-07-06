8b3d39c29f843cd9951228beed9568f8b0622fbf
Zack <beetle5249@gmail.com>
2026-07-06 15:31:33 +0800

    chores: change the implements of idnex

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from llm_ds import SingletonChatOpenAI
 from prompts import messages
+# langchain_core里有很多关键元素
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import PromptTemplate
 from pydantic import BaseModel, Field
@@ -19,8 +20,8 @@ llm = SingletonChatOpenAI().llm
 # print(output.content) # 其返
 
 
-#创建task
-class Task(BaseModel):
+#创建model，最终用在parser上，parser又在prompt上最终形成返回值的标准结构
+class ResModel(BaseModel):
     task_name: str = Field(description="分析任务，得到任务名称")
     task_type: str = Field(description="分析任务，得到任务的类型")
     task_content: str = Field(description="分析任务，得到任务的内容")
@@ -28,15 +29,15 @@ class Task(BaseModel):
     task_status: str = Field(description="分析任务，得到任务的状态,完成或未完成")
 
 
-# 定义一个任务查询字符串
-task_query = "如何在Cesium中集成Babylon？"
-
 # 需要一个parser，根据task确定返回值
-parser = JsonOutputParser(pydantic_object=Task)
+parser = JsonOutputParser(pydantic_object=ResModel)
 
-# 定义一个 PromptTemplate 实例，用于生成提示字符串
-# 该模板包含一个查询变量，用于在提示字符串中插入用户输入的查询
-# 该模板还包含一个格式化说明变量，用于在提示字符串中插入解析器生成的格式化说明
+
+# 定义一个任务查询字符串
+query = "如何在Cesium中集成Babylon？"
+
+
+# parser和query合成一个prompt，实际上我觉的这个PrmoptTemplate有点过于巨大了
 prompt = PromptTemplate(
     template="根据用户输入的问题得到任务JSON.\n{format_instructions}\n{query}\n",
     input_variables=["query"],
@@ -44,12 +45,12 @@ prompt = PromptTemplate(
     partial_variables={"format_instructions": parser.get_format_instructions()},
 )
 
-# taskchain
-task_chain = prompt | llm | parser
+# assemble as a chain，
+chain = prompt | llm | parser
 
 # taskchain.invoke执行
-task_data = task_chain.invoke({"query": task_query})
-print(task_data) # 这里得到的是解析过的结果，而不是message对象
+res = chain.invoke({"query": query})
+print(res) # 这里得到的是解析过的结果，而不是message对象
 
 
 

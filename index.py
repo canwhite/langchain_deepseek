47d3a53aec6db9ccaf2007917b5902fb83e51b02
Zack <beetle5249@gmail.com>
2026-07-06 15:24:37 +0800

    feat: see index py script

--- index.py
+++ index.py
@@ -19,9 +19,7 @@ llm = SingletonChatOpenAI().llm
 # print(output.content) # 其返
 
 
-
-# 定义一个名为 Task 的 Pydantic 模型，用于表示任务数据结构
-# 该模型包含五个字段：task_name、task_type、task_content、task_time 和 task_status
+#创建task
 class Task(BaseModel):
     task_name: str = Field(description="分析任务，得到任务名称")
     task_type: str = Field(description="分析任务，得到任务的类型")
@@ -33,8 +31,7 @@ class Task(BaseModel):
 # 定义一个任务查询字符串
 task_query = "如何在Cesium中集成Babylon？"
 
-# 初始化一个 JsonOutputParser 实例，用于解析模型生成的 JSON 输出
-# 并将其转换为 Task 模型实例
+# 需要一个parser，根据task确定返回值
 parser = JsonOutputParser(pydantic_object=Task)
 
 # 定义一个 PromptTemplate 实例，用于生成提示字符串
@@ -43,15 +40,14 @@ parser = JsonOutputParser(pydantic_object=Task)
 prompt = PromptTemplate(
     template="根据用户输入的问题得到任务JSON.\n{format_instructions}\n{query}\n",
     input_variables=["query"],
+    #上述parser就是为了确定返回json
     partial_variables={"format_instructions": parser.get_format_instructions()},
 )
 
-# 创建一个管道，将提示、模型和解析器连接在一起
-# 当调用该管道时，它将生成一个提示字符串，将其发送到模型以生成响应，
-# 然后将响应解析为 Task 模型实例
+# taskchain
 task_chain = prompt | llm | parser
 
-# 调用管道，使用用户输入的查询来生成任务数据
+# taskchain.invoke执行
 task_data = task_chain.invoke({"query": task_query})
 print(task_data) # 这里得到的是解析过的结果，而不是message对象
 
